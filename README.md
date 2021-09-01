2ad4e62acfe9462a075d071038d762c6b4ce1e7f
Antonio Sapuppo <antoniosapuppo@yahoo.it>
2021-09-01 19:02:42 +0200

    draft coding

--- README.md
+++ README.md
@@ -9,39 +9,37 @@ PIN 02
 PIN 03
 PIN 04
 PIN 05  3V3
-PIN 06  SDO     <-- D11 MOSI
-PIN 07  SDI     <-- D12 MISO
-PIN 08  SCLK    <-- D13 SCK
-PIN 09  NSEL    <-- D10 CS
-PIN 10  NIRQ    <-- D15
-PIN 11  SDN     <-- D5
+PIN 06  SDO     <-- GPIO 23/MOSI
+PIN 07  SDI     <-- GPIO 19/MISO
+PIN 08  SCLK    <-- GPIO 18/SCK
+PIN 09  NSEL    <-- GPIO 23/SS
+PIN 10  NIRQ    <-- GPIO 15
+PIN 11  SDN     <-- GPIO 0
 PIN 12
 ...
 PIN 13  ANTENNA
 PIN 14
 
-Arduino pro8MHzatmega328 3V3:
-PIN 31  TXD     per il USB converter    / serial sensor
-PIN 30  RXD     per il USB converter    / serial sensor
-
-RAW 5V0 <-- input from BEC
+LOLIN 32:
+RAW 5V0      <-- input from BEC
 GND GND common
-VCC 3V3 --> output to radio
-D13 SCK --> SLCK radio
-D12 MISO -> SDI radio
-D11 MOSI <- SDO radio
-D10 CS  --> NSEL radio
-D15     --> NIRQ
-D5      --> SDN
+VCC 3V3      --> output to radio
+GPIO 18/SCK  --> SLCK radio
+GPIO 19/MISO --> SDI radio
+GPIO 23/MOSI <-- SDO radio
+GPIO 23/SS   --> NSEL radio
+GPIO 15      --> NIRQ
+GPIO 0       --> SDN
 
 INGRESSI DIGITAL:
-D2  <-- RED BTN = HOME POSITION - FRONT VIEV
-D4  <-- BLACK BTN = COMMAND BY POTS
+GPIO 2  <-- RED BTN = HOME POSITION - FRONT VIEV
+GPIO 4  <-- BLACK BTN = COMMAND BY POTS
 
 INGRESSI ANALOGICI 0->3V3:
-A0  <-- RP = PAN
-A3  <-- LP = TILT
+ADC1_0  <-- RP = PAN
+ADC1_3  <-- LP = TILT
 
 ## interfaces
 
 MQTT messages
+console menu
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,205 @@
+/*! @mainpage
+//-------------------------------------------------------------------------------
+*/
 #include <Arduino.h>
 
+//-- DEBUG ----------------------------------------------------------------------
+
+//-- CONFIGURATIONS -------------------------------------------------------------
+#define UART_ECHO               (0)
+#define UART_BAUDRATE           (19400)
+
+#define ADC1_0                  (36)
+#define ADC1_3                  (39)
+
+//-- 433 MHz RADIO --------------------------------------------------------------
+#include <RadioLib.h>
+
+// https://www.electrodragon.com/w/Si4432
+// Si4432 has the following connections:
+#define RADIO_nSEL      10
+#define RADIO_nIRQ      2
+#define RADIO_SDN       5
+
+Si4432 radio = new Module(RADIO_nSEL, RADIO_nIRQ, RADIO_SDN);
+
+//-- MSG PACK -------------------------------------------------------------------
+// RX PROTOCOL
+uint8_t pan;
+uint8_t tilt;
+uint8_t outd;   // bit 3 = PIN 3, pin 4 = PIN 4
+#define PACKET_RX_SIZE (sizeof(uint8_t) * 3)
+// TX PROTOCOL
+float analogPan;
+float analogTilt;
+// #define MSGPACK_DEBUGLOG_ENABLE
+#include <MsgPack.h>
+
+MsgPack::Packer packer;
+MsgPack::Unpacker unpacker;
+
+//--- CONSOLE MENU ---------------------------------------------------------------
+// https://github.com/neu-rah/ArduinoMenu/wiki/Menu-definition
+#include <menu.h>
+#include <menuIO/serialOut.h>
+#include <menuIO/chainStream.h>
+#include <menuIO/serialIn.h>
+
+using namespace Menu;
+
+#define MAX_DEPTH   1
+
+result menuShowIP();
+result menuListAP();
+result menuSetUser();
+result menuSetPass();
+result menuSave();
+result menuInfo();
+
+MENU(mainMenu,"telemetry config",doNothing,noEvent,wrapStyle
+  ,OP("show IP",menuShowIP,enterEvent)
+  ,OP("list AP",menuListAP,enterEvent)
+  ,OP("set user",menuSetUser,enterEvent)
+  ,OP("set password",menuSetPass,enterEvent)
+  ,OP("save",menuSave,enterEvent)
+  ,OP("nav info",menuInfo,enterEvent)
+  ,EXIT("<Back")
+);
+
+serialIn serial(Serial);
+MENU_INPUTS(in,&serial);
+
+MENU_OUTPUTS(out,MAX_DEPTH
+  ,SERIAL_OUT(Serial)
+  ,NONE//must have 2 items at least
+);
+
+NAVROOT(nav,mainMenu,MAX_DEPTH,in,out);
+
+
+//-------------------------------------------------------------------------------
+#include <TaskScheduler.h>
+
+void sensorReading();
+Task sensorTask(5000, TASK_FOREVER, &sensorReading);
+Scheduler runner;
+
+float voltageReading(float value) {
+  return ((5*value)/1023)*4.103354632587859;
+}
+
+void sensorReading() {
+  analogPan = voltageReading(ADC1_0);
+  analogTilt = voltageReading(ADC1_3);
+  packer.serialize(analogPan, analogTilt);
+  int state = radio.transmit((uint8_t *)packer.data(), packer.size());
+
+  if (state == ERR_NONE) {
+    // the packet was successfully transmitted
+    Serial.println(F("I:Si4432:TX:success!"));
+
+  } else if (state == ERR_PACKET_TOO_LONG) {
+    // the supplied packet was longer than 256 bytes
+    Serial.println(F("I:Si4432:TX:too long!"));
+
+  } else if (state == ERR_TX_TIMEOUT) {
+    // timeout occured while transmitting packet
+    Serial.println(F("I:Si4432:TX:timeout!"));
+
+  } else {
+    // some other error occurred
+    Serial.print(F("I:Si4432:TX:failed, code "));
+    Serial.println(state);
+  }
+}
+
+//-------------------------------------------------------------------------------------------------------
+
+result menuShowIP() {
+  Serial.println("menuShowIP");
+  return proceed;
+}
+
+result menuListAP() {
+  Serial.println("menuListAP");
+  return proceed;
+}
+
+result menuSetUser() {
+  Serial.println("menuSetUser");
+  return proceed;
+}
+
+result menuSetPass() {
+  Serial.println("menuSetPass");
+  return proceed;
+}
+
+result menuSave() {
+  Serial.println("menuSave");
+  return proceed;
+}
+
+result menuInfo() {
+  Serial.println("I:CONSOLE");
+  Serial.println("I:Use keys [+ up] [- down] [* enter] [/ esc]");
+  Serial.println("I:to control the menu navigation");
+  return proceed;
+}
+
+//-------------------------------------------------------------------------------
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(UART_BAUDRATE);
+  // needed to keep leonardo/micro from starting too fast!
+  while (!Serial) { delay(10); }
+
+  Serial.print(F("I:Si4432:START"));
+  int state = radio.begin();
+  if (state == ERR_NONE) {
+    Serial.println(F("I:Si4432:success!"));
+  } else {
+    Serial.print(F("I:Si4432:failed, code"));
+    Serial.println(state);
+    while (true);
+  }
+
+  runner.init();
+  runner.addTask(sensorTask);
+  sensorTask.enable();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  byte payload[PACKET_RX_SIZE];
+  int state = radio.receive(payload, PACKET_RX_SIZE);
+
+  if (state == ERR_NONE) {
+    // packet was successfully received
+    Serial.println(F("I:Si4432:RX:success!"));
+    unpacker.feed(payload, PACKET_RX_SIZE);
+    unpacker.deserialize(pan, tilt, outd);
+
+    // print the data of the packet
+    Serial.print(F("I:Si4432:RX:Data:\t\t"));
+    Serial.println((char*)payload);
+
+  } else if (state == ERR_RX_TIMEOUT) {
+    // timeout occurred while waiting for a packet
+    Serial.println(F("I:Si4432:RX:timeout!"));
+
+  } else if (state == ERR_CRC_MISMATCH) {
+    // packet was received, but is malformed
+    Serial.println(F("I:Si4432:RX:CRC error!"));
+
+  } else {
+    // some other error occurred
+    Serial.print(F("I:Si4432:RX:failed, code "));
+    Serial.println(state);
+
+  }
+  runner.execute();
+
+  nav.doInput();
+  if (nav.changed(0)) {
+    nav.doOutput();
+  }
 }
